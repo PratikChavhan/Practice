714c7168a651f0576c4bdfe71f78b5f9f9f5dbf7
Pratik <pratik.chavhan.77@gmail.com>
2023-11-08 08:31:33 +0530

    collections practice

---
+++ Java/Collections/CollectionsPractice/src/com/demo/stack/TestStack.java
@@ -0,0 +1,34 @@
+package com.demo.stack;
+
+import java.util.Stack;
+
+public class TestStack {
+	public static void main(String[] args) {
+		
+	// Stack Declaration
+		Stack<String> animals = new Stack<>();
+		
+		
+	// Adding elements in stack
+		animals.push("Lion");
+		animals.push("Tiger");
+		animals.push("Horse");
+		animals.push("Dog");
+		
+		
+	// Display stack
+		// System.out.println("Stack: " + animals);
+		
+	
+	// Peek ==> Display last inserted element
+		// System.out.println(animals.peek());
+	
+	
+	// Pop ==> removes last inserted element in stack
+		// System.out.println(animals.pop());
+		// System.out.println(animals.peek());
+		
+		
+	// 
+	}
+}
