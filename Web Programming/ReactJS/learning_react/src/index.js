9356284d4604746c7bc4eaafabf6e625149bc439
Pratik <pratik.chavhan.77@gmail.com>
2023-12-18 19:37:30 +0530

    dropdowm useState

--- Web Programming/ReactJS/learning_react/src/index.js
+++ Web Programming/ReactJS/learning_react/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 // import App1 from './Apps/App1'
-import App2 from './Apps/App2'
+// import App2 from './Apps/App2'
+import App3 from './Apps/App3'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <App />
     // <App1 />
-    <App2 />
+    // <App2 />
+    <App3 />
 )
