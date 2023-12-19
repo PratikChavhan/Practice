bd86a7c99f25ee9bd4094e5509b3c949c144a616
Pratik <pratik.chavhan.77@gmail.com>
2023-12-19 07:11:30 +0530

    Class Component 1

---
+++ Web Programming/ReactJS/learning_react/src/Apps/App4.js
@@ -0,0 +1,11 @@
+import HalfString from "../extras/HalfString";
+
+function App4() {
+    return (
+        <>
+            <HalfString></HalfString>
+        </>
+    )
+}
+
+export default App4;
--- Web Programming/ReactJS/learning_react/src/index.js
+++ Web Programming/ReactJS/learning_react/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 // import App from './App';
 // import App1 from './Apps/App1'
 // import App2 from './Apps/App2'
-import App3 from './Apps/App3'
+// import App3 from './Apps/App3'
+import App4 from './Apps/App4'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <App />
     // <App1 />
     // <App2 />
-    <App3 />
+    // <App3 />
+    // <App4 />
+    <App5 />
 )
